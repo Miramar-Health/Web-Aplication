d57e22e543369b4a9419e39d608a022115abf947
MATEUS GIDEAO DE SOUZA BARBOSA <mateus.gsbarbosa@senacsp.edu.br>
2019-09-17 14:47:34 -0300

    inicio aula 04 php na uc12

--- index.php
+++ index.php
@@ -1,1 +1,55 @@
-<!-- Uc12 -->
+<?php
+//variaveis no PHP
+$nome = "Mateus"
+echo $nome;
+echo "<br>";
+$nome +='Gideao';
+echo $nome;
+echo "<br>";
+$nome = "Mat" . "'eus'". '"Gideão"';
+echo $nome;
+echo "<br>"
+var_dump($nome);
+echo "<br>"
+$idade = 10;
+echo $idade;
+echo "<br>";
+var_dump($idade);
+echo "<br>"
+
+
+$teste = null;
+echo $teste;
+echo "<br>";
+var_dump($teste);
+echo "<br>"
+
+
+$valor = 125.65;
+echo $valor;
+echo "<br>";
+var_dump($valor);
+echo "<br>"
+
+
+echo $idade*$valor;
+
+echo "<br>";
+
+echo 10*3/2**4-7+15; //ordem de precedencia de operadores aritimeticos
+
+echo "<br>"
+
+?>
+<!DOCTYPE html>
+<html lang="Pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <title>Aula Uc12 Php</title>
+</head>
+<body>
+    
+</body>
+</html>
